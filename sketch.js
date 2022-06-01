5196473bff04badce6816ba5446605b9ebda6615
keith yu <34111214+kyu3@users.noreply.github.com>
2022-06-01 18:34:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+var x;
+var y;
+var radius;
+var speed;
+
+
 function setup() {
   // put setup code here
+ CreateCanvas size(400,400);
+ frameRate(15);
+ x=width/2;
+ y=25;
+ radius=50;
+ speed =5;
+ background(255);
 }
 
 function draw() {
   // put drawing code here
+}
+
+function circ( x,  y, radius){
+ circle(x, y, radius/2); 
 }
